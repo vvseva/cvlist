d20802de1a80f4a49227986b9609da7aad76f0c7
vvseva <wirzet1@gmail.com>
2022-03-02 17:03:13 +0300

    mvp

---
+++ cvlist_app/server.R
@@ -0,0 +1,45 @@
+library(shiny)
+library("googlesheets4")
+library("shiny")
+library(DT)
+library(dplyr)
+library(stringr)
+
+gs4_auth(path = ".secrets/cvlist-5a763ac3f721.json")
+
+to_href <- function(link, link_text){
+  str_c(
+    "<a href='", {{link}}, "'>", {{link_text}}, "</a>")
+}
+
+to_mail <- function(mail){
+  str_c(
+    "<a href='mailto:", {{mail}}, "'>", {{mail}}, "</a>")
+}
+
+server <- function(input, output, session) {
+  output$tbl <- read_sheet(
+    "https://docs.google.com/spreadsheets/d/1WimbMiZD38FyCeek5Rxj7LnLW-ii7Yl55jBzQTN4DKE/edit?usp=sharing",
+    range = "Candidates"
+  ) %>% 
+    select(-Status) %>% 
+    mutate(LinkedIn = to_href(LinkedIn, LinkedIn),
+           `Resume/CV` = to_href(`Resume/CV`, "CV"),
+           email = to_mail(email),
+           Telegram = to_href(str_c("https://telegram.me/", Telegram), Telegram)
+           ) %>% 
+  renderDT(
+    filter = 'top',
+    escape = FALSE,
+    options = list(pageLength = 100, autoWidth = TRUE)
+  )
+  
+  # observeEvent(input$add, {
+  #   df <-read_sheet("https://docs.google.com/spreadsheets/d/150R62xJnOOcYebyEZNUwhXHzjfCeyx2QwRdTuMU-5Gg/edit?usp=sharing"
+  #   )
+  #   output$df <- renderTable(df)
+  # })
+  
+}
+
+shinyServer(server)
---
+++ cvlist_app/ui.R
@@ -0,0 +1,27 @@
+library("googledrive")
+library("googlesheets4") # I am using the developing version 0.1.0.9000
+library("shiny")
+library(DT)
+# library("kableExtra")
+
+
+ui <- # Define UI for application that plots random distributions
+  fluidPage(# Application title
+    titlePanel("Candidates"),
+    fluidRow(
+      column(
+        6,
+        
+        tags$div(class="header", checked=NA,
+                 tags$p("With the current situation around Ukraine and Russia, lots of my university friends in Saint Petersburg are highly motivated to find a job abroad."),
+                 tags$a(href="https://www.linkedin.com/posts/anna-iashina_datascience-analytics-ml-activity-6904429272870608896-AWw4", 
+                        "Original post, please read")
+        )
+      )
+    ),
+    fluidRow(),
+    # Show a plot of the generated distribution
+    fluidRow(column(
+      10,
+      DTOutput("tbl")
+    )))
